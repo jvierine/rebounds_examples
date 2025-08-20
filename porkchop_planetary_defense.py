95522cb7ca3a9e4f93e72da66a559b8f394f8067
juha <you@example.com>
2025-08-20 21:41:18 +0200

    asd

--- porkchop_planetary_defense.py
+++ porkchop_planetary_defense.py
@@ -47,13 +47,11 @@ for i in range(n_tof):
     re,ve=earth.eph(pk.epoch(departure_time[i], 'mjd2000'))
 
     for j in range(n_tof):
-        # minimum travel time
         # reach target 30 days before impact
         if (arrival_time[j]>(departure_time[i]+10)) and arrival_time[j]<(t_impact-30) :
             rc,vc=comet.eph(pk.epoch(arrival_time[j], 'mjd2000'))
             tof=arrival_time[j]-departure_time[i]
             dt=tof*pk.DAY2SEC
-            #print(dt)
             l = pk.lambert_problem(r1 = re, r2 = rc, tof = dt, mu = pk.MU_SUN, max_revs=2,cw=False)
             exit_dv_cw=np.linalg.norm(np.array(ve)-np.array(l.get_v1()[0]))
             if exit_dv_cw < best_dv:
@@ -61,73 +59,57 @@ for i in range(n_tof):
                 best_dv=exit_dv_cw
                 best_res={"l":l,"t0":departure_time[i],"t1":arrival_time[j],"tof":dt,"re":re}
                 print("found better delta v %1.2f (km/s)"%(best_dv/1e3))
-
             delta_v[0,i,j]=exit_dv_cw
 
+# porkchop plot
 plt.pcolormesh(departure_time,arrival_time,delta_v[0,:,:].T/1e3,cmap="turbo",vmin=0,vmax=50)
 plt.title("Prograde orbits")
-
 cb=plt.colorbar()
 cb.set_label(r"$\Delta v$ (km/s)")
 plt.xlabel("Departure time (days)")
 plt.ylabel("Arrival time (days)")
-
 plt.show()
 
 # plot one lambert solution
 t_exit=best_res["t0"]
 t_arrival=best_res["t1"]
+# position of earth and comet at launch and impact
 re,ve=earth.eph(pk.epoch(t_exit, 'mjd2000'))
 rc,vc=comet.eph(pk.epoch(t_arrival, 'mjd2000'))
 
+# solve the Lambert problem for the best solution
 l = pk.lambert_problem(r1 = re, r2 = rc, tof = best_res["tof"], mu = pk.MU_SUN, max_revs=2,cw=False)
-
 rocket_v=l.get_v1()[0]
 rocket_r=l.get_x()[0]
-print("rocket r")
-print(rocket_r)
-print("rocket v")
-print(rocket_v)
-print("earth r")
-print(re)
-print("earth v")
-print(ve)
-#orb = pk.orbit_elements(re, rocket_v, mu_sun)
-#print(orb)
 rocket = pk.planet.keplerian(pk.epoch(t_exit, 'mjd2000'),re, rocket_v, pk.MU_SUN, 10, 10, 10,  'Rocket')
-print(rocket)
 
+# plot orbits of Earth and the impacting comet until impact time
 times = np.linspace(-100, t_impact, 10000)
 rs=[]
 crs=[]
-
 for t in times:
-#    print(t)
     r,v=earth.eph(pk.epoch(t,"mjd2000"))
     rs.append(r)
     r,v=comet.eph(pk.epoch(t,"mjd2000"))
     crs.append(r)
 
+# figure out where the rocket is between launch and impact times
 times = np.linspace(t_exit, t_arrival, 1000)
 rrocket=[]
 for t in times:
     r,v=rocket.eph(pk.epoch(t,"mjd2000"))
-#    print(r)
     rrocket.append(r)
-
+# convert to numpy arrays
 rs = np.array(rs)
 crs = np.array(crs)
 rrocket = np.array(rrocket)
-
+# plot positions
 plt.figure(figsize=(6,6))
 plt.plot(rs[:,0]/pk.AU, rs[:,1]/pk.AU, label="Earth orbit")
 plt.plot(crs[:,0]/pk.AU, crs[:,1]/pk.AU, label="Comet")
-
 plt.plot(rrocket[:,0]/pk.AU, rrocket[:,1]/pk.AU, label="Rocket")
-
 plt.plot(crs[-1,0]/pk.AU, crs[-1,1]/pk.AU, "*", label="Comet impacts Earth")
 plt.plot(rrocket[-1,0]/pk.AU, rrocket[-1,1]/pk.AU, "*", label="Rocket impacts comet")
-
 plt.scatter([0],[0],c='orange',marker='*',s=200,label="Sun")
 plt.axis('equal')
 plt.xlabel("x [AU]")
